eb6102913b31675292c202d7b4f6be55433e1f2a
Mhbib34 <mhabib34official@gmail.com>
2025-02-17 22:16:04 +0700

    Menhapus file input.jsx

--- src/components/Fragments/FormSearch.jsx
+++ src/components/Fragments/FormSearch.jsx
@@ -1,14 +1,12 @@
-import Input from "../common/Input";
-
 export default function FormSearch() {
   return (
     <div className="flex gap-2 items-center w-full">
       <i className="text-white text-2xl bx bx-search"></i>
-      <Input
+      <input
         type="text"
         name="search-input"
         placeholder="Search here..."
-        className="focus:outline-none "
+        className="focus:outline-none  placeholder:text-white p-1 rounded-md px-2 w-full text-white "
       />
     </div>
   );
