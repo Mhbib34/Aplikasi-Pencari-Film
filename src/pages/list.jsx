b009db4ecb228aff21a625e000acc4f9b6d23d1d
Mhbib34 <mhabib34official@gmail.com>
2025-02-20 16:06:30 +0700

    Menambahkan responsive

--- src/pages/list.jsx
+++ src/pages/list.jsx
@@ -4,36 +4,38 @@ export default function WatchList() {
   const watchList = JSON.parse(localStorage.getItem("watchList") || []);
 
   return (
-    <div className="px-5 py-14 flex gap-3 flex-col">
+    <div className="px-5 lg:px-20 py-14 flex gap-3 flex-col">
       <div className="">
         <span className="border py-1 px-2 bg-primary text-secondary rounded-md shadow-[2px_2px_2px_rgba(0,0,0,1)] ">
           Watch List
         </span>
       </div>
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col lg:flex-row gap-5 lg:flex-wrap justify-between">
         {watchList.map((item) => (
-          <div className="flex gap-4 h-44 border shadow-[2px_2px_2px_rgba(0,0,0,1)] items-center ">
-            <div className="w-[50%] h-full">
+          <div className="flex gap-4 lg:flex-col lg:w-60  lg:h-auto h-44 border shadow-[2px_2px_2px_rgba(0,0,0,1)] items-center ">
+            <div className="w-[50%] lg:h-[65%] lg:w-full h-full ">
               <img
                 src={item.Poster}
                 alt={item.Title}
                 className="w-full h-full"
               />
             </div>
-            <div className="flex flex-col w-[50%] justify-between h-full py-2 gap-2 px-1">
-              <span className="text-sm font-medium">{item.Title}</span>
+            <div className="flex flex-col w-[50%] lg:h-[35%] lg:justify-between  lg:w-full justify-between h-full py-2 gap-2 px-1 lg:items-center">
+              <span className="text-sm font-medium lg:text-lg lg:text-center">
+                {item.Title}
+              </span>
               <span className="text-sm font-medium">{item.Genre}</span>
               <span className="font-medium">⭐{item.imdbRating}</span>
-              <div className="flex gap-2">
+              <div className="flex gap-2 w-full justify-center">
                 <Button
                   type="button"
                   text="Done!"
-                  className="bg-primary text-secondary py-1"
+                  className="bg-primary text-secondary py-1 lg:w-[40%]"
                 />
                 <Button
                   type="button"
                   text="Deleted"
-                  className="bg-secondary text-primary py-1"
+                  className="bg-secondary text-primary py-1 lg:w-[40%]"
                 />
               </div>
             </div>
