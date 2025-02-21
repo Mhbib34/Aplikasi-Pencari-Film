c910fd2deb6c329eb64357af3be3e2dc7a8dce53
Mhbib34 <mhabib34official@gmail.com>
2025-02-21 19:09:31 +0700

    Menambahkan fitur menandai selesai menonton pada halaman watchList

--- src/pages/list.jsx
+++ src/pages/list.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Button from "../components/common/Button";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Swal from "sweetalert2";
 
 export default function WatchList() {
@@ -9,62 +9,68 @@ export default function WatchList() {
     return storedWatchlist;
   });
 
-  function handleRemoveItem(imdbID) {
-    Swal.fire({
-      title: "Deleted Movie",
-      text: "Are you sure you want to delete the movie?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#bab8f3",
-      cancelButtonColor: "#2b2b2b",
-      confirmButtonText: "Deleted",
-      cancelButtonText: "Cancel",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          title: "Success",
-          text: "Movie deleted Successfully",
-          icon: "success",
-          iconColor: "#bab8f3",
-          confirmButtonColor: "#bab8f3",
-          confirmButtonText: "Close",
-        });
-        const filteredData = watchList.filter((item) => item.imdbID !== imdbID);
-        setWatchList(filteredData);
-        localStorage.setItem("watchList", JSON.stringify(filteredData));
-      }
-    });
-  }
+  const updateLocalStorage = (data) => {
+    localStorage.setItem("watchList", JSON.stringify(data));
+  };
 
-  function handleRemoveAllItem() {
+  const handleDoneWatching = useCallback(
+    (imdbID) => {
+      const updatedWatchList = watchList.map((item) =>
+        item.imdbID === imdbID ? { ...item, done: !item.done } : item
+      );
+      setWatchList(updatedWatchList);
+      updateLocalStorage(updatedWatchList);
+    },
+    [watchList]
+  );
+
+  const handleRemoveItem = useCallback(
+    (imdbID) => {
+      Swal.fire({
+        title: "Delete Movie",
+        text: "Are you sure you want to delete this movie?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#bab8f3",
+        cancelButtonColor: "#2b2b2b",
+        confirmButtonText: "Delete",
+        cancelButtonText: "Cancel",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const filteredData = watchList.filter(
+            (item) => item.imdbID !== imdbID
+          );
+          setWatchList(filteredData);
+          updateLocalStorage(filteredData);
+          Swal.fire("Success", "Movie deleted successfully", "success");
+        }
+      });
+    },
+    [watchList]
+  );
+
+  const handleRemoveAllItem = useCallback(() => {
     Swal.fire({
-      title: "Deleted All Movies",
+      title: "Delete All Movies",
       text: "Are you sure you want to delete all movies?",
       icon: "warning",
       showCancelButton: true,
       confirmButtonColor: "#bab8f3",
       cancelButtonColor: "#2b2b2b",
-      confirmButtonText: "Deleted",
+      confirmButtonText: "Delete All",
       cancelButtonText: "Cancel",
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire({
-          title: "Success",
-          text: "All Movies deleted Successfully",
-          icon: "success",
-          iconColor: "#bab8f3",
-          confirmButtonColor: "#bab8f3",
-          confirmButtonText: "Close",
-        });
         setWatchList([]);
-        localStorage.setItem("watchList", JSON.stringify([]));
+        updateLocalStorage([]);
+        Swal.fire("Success", "All movies deleted successfully", "success");
       }
     });
-  }
+  }, []);
 
   return (
     <div className="px-5 lg:px-20 py-14 flex gap-3 flex-col">
-      <div className="flex gap-3">
+      <div className="flex gap-3 mb-4">
         <Link to="/">
           <Button
             text="Home"
@@ -84,12 +90,17 @@ export default function WatchList() {
               key={index}
               className="flex gap-4 lg:flex-col lg:w-60  lg:h-auto h-44 border shadow-[2px_2px_2px_rgba(0,0,0,1)] items-center "
             >
-              <div className="w-[50%] lg:h-[65%] lg:w-full h-full ">
+              <div className="w-[50%] lg:h-[65%] lg:w-full h-full relative ">
                 <img
                   src={item.Poster}
                   alt={item.Title}
                   className="w-full h-full"
                 />
+                {item.done && (
+                  <div className="w-10 h-10 bg-primary text-white flex justify-center items-center rounded-full text-xl shadow-lg absolute -top-3 -left-3">
+                    ✔️
+                  </div>
+                )}
               </div>
               <div className="flex flex-col w-[50%] lg:h-[35%] lg:justify-between  lg:w-full justify-between h-full py-2 gap-2 px-2 lg:items-center">
                 <span className="text-sm font-medium lg:text-lg lg:text-center">
@@ -101,8 +112,9 @@ export default function WatchList() {
                 <div className="flex w-full justify-between lg:justify-center lg:gap-2">
                   <Button
                     type="button"
-                    text="Done!"
+                    text={item.done ? "Undo" : "Done!"}
                     className="bg-primary text-secondary py-1 lg:w-[40%]"
+                    onClick={() => handleDoneWatching(item.imdbID)}
                   />
                   <Button
                     type="button"
